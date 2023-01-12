cc9cf6280500b31bf373e5f93dc90ce49f7f66dc
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2023-01-12 10:52:25 +0100

    Vorlesung 2023-01-12

--- 2022-12-02/ex_pca.py
+++ 2022-12-02/ex_pca.py
@@ -4,11 +4,30 @@ Informationen über die Daten (Places Rated Almanac, by Richard Boyer and David 
 import numpy as np
 data = np.loadtxt('data/places.csv', delimiter=',', skiprows=1)
 
-print(data[:, 1:10])
+# print(data[:1, 1:10])
+
+# Eigenleistung
+from sklearn.decomposition import PCA
+pca = PCA()
+pca.fit(data[:, 1:10])
+
+print(pca.explained_variance_ratio_)
+
+from itertools import accumulate
+print([i for i in accumulate(pca.explained_variance_ratio_)])
+
+
+# Gibt die Komponenten (alle) von den ersten 4 ersten und besten Vektoren zurück
+print(pca.components_[:4, :])
 
 """
 Aufgaben
 1) Welche Hauptkomponenten werden benötigt? Was sagen diese aus?
+Hauptkomponent = vektor
+97% reichen in dem Fall, weshalb die ersten 4 Hauptkomponenten benutzt werden
+
 2) Mit welchen Variablen kann der Datensatz am einfachsten erklärt werden?
-3) Welche Städte haben ungewöhnliche Daten? (z.B. mit matplotlib)
+recreat, healthcare, educ, casenum
+
+# 3) Welche Städte haben ungewöhnliche Daten? (z.B. mit matplotlib)
 """
